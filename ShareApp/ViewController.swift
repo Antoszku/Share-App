d72a7b2509f744a6db0a93a0c39e691301d5fb59
Dominik Rygiel <dominik.rygiel@codahead.com>
2019-04-01 11:39:49 +0200

    Initial Commit

---
+++ ShareApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareApp
+//
+//  Created by Dominik Rygiel on 01/04/2019.
+//  Copyright © 2019 Dominik Rygiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
